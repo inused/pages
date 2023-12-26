663b7fc33bacf7901006c185a9f08eda5eee80fa
inused <inused@pc>
2023-12-26 15:08:11 +0800

    添加简单的反向代理

---
+++ functions/reversal.js
@@ -0,0 +1,20 @@
+import reqUtil from './util/reqUtil.js';
+import resUtil from './util/resUtil.js';
+
+/**
+ * 
+ * 反向代理
+ * 
+ * @param {Context} cfContext Cloudflare pages 上下文
+ * @returns {Response}
+ */
+export async function onRequest(cfContext) {
+  // 反代目标
+  const target = reqUtil.getSearchParam("target");
+  if (!target) {
+    return resUtil.initResponse(500, 500, 'target is required');
+  }
+
+  // 当前仅转发, 不对其他数据进行修改(如host/referer等)
+  return fetch(new Request(target, cfContext.request))
+}
