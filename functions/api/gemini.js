1d30701fea4cafb90f4ffae5064b399a12dfa05d
inused <inused@pc>
2023-12-26 14:37:01 +0800

    添加对 Google Gemini Pro API 的转发

---
+++ functions/api/gemini.js
@@ -0,0 +1,16 @@
+import authUtil from '../util/authUtil.js'
+
+/**
+ * 转发 Google Gemini Pro 的api请求
+ * @param {Context} cfContext Cloudflare pages 上下文
+ * @returns {Response}
+ */
+export async function onRequest(cfContext) {
+  const authRes = authUtil.tokenAuthResponse(cfContext);
+  if (authRes) return authRes; // 有返回值说明验证未通过
+
+  // 转发到
+  const url = new URL(cfContext.request.url);
+  url.host = 'generativelanguage.googleapis.com';
+  return fetch(new Request(url, cfContext.request))
+}
